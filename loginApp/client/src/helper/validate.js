3e7551fe8deea7461fa761e6139441a86f868818
Norah Almaneea <nouraalmanea0@gmail.com>
2023-05-03 12:06:47 +0300

    Recovry & reset UI

--- loginApp/client/src/helper/validate.js
+++ loginApp/client/src/helper/validate.js
@@ -6,12 +6,45 @@ export async function usernameValidate(values){
     return errors;
 }
 
-/** validate username */
+export async function passwordValidate(values){
+    const errors = passwordVerify({}, values)
+    return errors;
+}
+
+export async function resetPasswordValidate(values){
+    const errors = passwordVerify({}, values)
+
+    if(values.password !== values.confirm_pwd){
+        errors.exist = toast.error('Password Not Match!');
+    }
+    return errors;
+}
+
+/** Validate Password Reset */
+
+
+/** Validate Password */
+function passwordVerify(error = {}, values){
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    if(!values.password){
+        error.password = toast.error('Password Required!');
+    }else if(values.password.includes(" ")){
+        error.password = toast.error('Wrong Password!')
+    }else if(values.password.length < 4){
+        error.password = toast.error('Password Must Be More Than 4 Character')
+    } else if(!specialChars.test(values.password)){
+        error.password = toast.error("Password must have special character");
+    }
+
+    return error;
+}
+
+/** Validate username */
 function usernameVerify(error = {}, values){
     if(!values.username){
-        error.username = toast.error('Username Required...!');
+        error.username = toast.error('Username Required!');
     }else if(values.username.includes(" ")){
-        error.username = toast.error('Invalid Username...!')
+        error.username = toast.error('Invalid Username!')
     }
 
     return error;
